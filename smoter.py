20733d5af8777cf862e7fb523e858c6d4c981d80
slickardo <ricardojoseromero@gmail.com>
2018-12-05 19:32:07 -0500

    Added printToCSV functions in smoter. uploading two balanced csvs

--- smoter.py
+++ smoter.py
@@ -18,6 +18,18 @@ def loadYcsv():
 			ret_list.append(row)
 	return ret_list
 
+def printXTocsv(x):
+	with open('balanced_clean_x_all.csv', 'w') as csvfile:
+		csvWriter = csv.writer(csvfile)
+		for row in x:
+			csvWriter.writerow(row)
+
+def printYTocsv(y):
+	with open('balanced_clean_y_all.csv', 'w') as csvfile:
+		csvWriter = csv.writer(csvfile)
+		for row in y.tolist():
+			csvWriter.writerow([row])	
+
 def smoteData(x,y):
 	value, count = np.unique(y, return_counts=True)
 	counts = dict(zip(value, count))
@@ -31,13 +43,16 @@ def smoteData(x,y):
 	counts = dict(zip(value, count))
 	print("After SMOTE")
 	print(counts)
+	return x,y
 
 def main():
 	x = np.array(loadXcsv()).astype(np.float64)
 	y = np.array(loadYcsv()).astype(np.float64)
 	y = np.ravel(y)
 	
-	smoteData(x,y)
+	x,y = smoteData(x,y)
+	printXTocsv(x)
+	printYTocsv(y)
 
 if __name__ == '__main__':
 	main()
